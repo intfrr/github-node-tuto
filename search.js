685ce063a5c094fa69ab05eabd830fdc245ec516
Alejandro Alcalde <algui91@gmail.com>
2013-11-01 10:50:21 +0100

    Formating output

--- search.js
+++ search.js
@@ -4,9 +4,9 @@ var github = require("github");
 var path = require('path');
 
 /**
- * Check is user pass the right params
+ * Check if user pass the right params
  */
-if ( process.argv.length <= 4 ) {
+if ( process.argv.length <= 3 ) {
        return console.log("Usage: " + path.basename(process.argv[1]) + " <location> " + "<user_token>");
 };
 
@@ -31,16 +31,32 @@ gh.authenticate({
 gh.search.users({
     keyword: location
 }, function(err, res) {
-    console.log( res) ;
+    console.log("#################################################\n" +
+                "#################################################");
+    console.log("Usuarios de " + arg + " y sus repositorios");
+    console.log("#################################################\n" +
+                "#################################################");
+    for (i = 0 ; i < res.users.length; i++){
+        
+        gh.repos.getFromUser({
+            user: res.users[i].username,
+            type: "owner",
+            sort: "created"
+        }, function(err, response){
+            if (response[0] != undefined 
+                && response[0].hasOwnProperty("owner") 
+                && response[0].owner.hasOwnProperty("login")){
+                    console.log("------------------------------------------");
+                    console.log(response[0].owner.login + " ( " + response[0].owner.html_url + " )" );
+                }
+            for ( k = 0 ; k < response.length ;k++){
+                var lenguaje = response[k].language === "null" ? "No definido" : response[k].language;
+                console.log("\t" + response[k].name + "\n" +
+                    "\t\tLenguaje:" + lenguaje + "\n" + 
+                    "\t\tNúmero de forks:" + response[k].forks_count + "\n" +
+                    "\t\tSeguido por:" + response[k].watchers + " Personas\n");
+            }
+       });
+    }
 });
 
-gh.repos.getFromUser({
-    user: "algui91",
-    type: "owner",
-    sort: "created"
-}, function(err, res){
-    if (err) {
-        return console.log(err);
-    };
-    console.log(res);
-});
